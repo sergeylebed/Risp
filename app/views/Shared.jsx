3b195a6b5ef0f2c1073e06234b3e9677b1b81411
Anna <annlebed2010@gmail.com>
2016-02-20 16:43:01 +0300

    Add logo and menu

--- app/views/Shared.jsx
+++ app/views/Shared.jsx
@@ -6,17 +6,40 @@ export default function Shared({ context, children }) {
   return (
     <div>
       <div className='row'>
-        <div className='navbar navbar-default'>
+        <nav className='navbar navbar-default navbar-fixed-top navbar-top'>
           <div className='container'>
-            <a className='navbar-brand' href='#' onClick={(e) => { e.preventDefault(); context.redirect('#/Welcome'); }}>Risp</a>
+            <ul className="nav navbar-nav">
+              <li><a href='#' onClick={(e) => { e.preventDefault(); context.redirect('#/Welcome'); }}><span className="glyphicon glyphicon-home" aria-hidden="true"></span></a></li>
+              <li>
+                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span className="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span></a>
+                <ul className="dropdown-menu">
+                  <li><a href="#">Open</a></li>
+                  <li><a href="#">Save</a></li>
+                  <li></li>
+                </ul>
+              </li>
+             </ul>
+            <div className="navbar-header">
+              <a className='navbar-brand' href='#' onClick={(e) => { e.preventDefault(); context.redirect('#/Welcome'); }}><img src="images/logo.png" alt="RISP" /></a>
+            </div>
           </div>
-        </div>
+        </nav>
       </div>
       <div className='row'>
         <div className='container'>
           {
             clonedChildren
           }
+        </div>
+      </div>
+      <div className='row'>
+        <div className='navbar navbar-default'>
+          <div className='container'>
+            <ul className="nav navbar-nav">
+              <li><a href='#' onClick={(e) => { e.preventDefault(); context.redirect('#/Welcome'); }}>About</a></li>
+              <li><a href='#' onClick={(e) => { e.preventDefault(); context.redirect('#/Welcome'); }}>Tell us</a></li>
+             </ul>
+          </div>
         </div>
       </div>
     </div>
