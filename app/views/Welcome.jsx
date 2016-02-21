dba9f4dac145a3b650aa6a9e2d9d4c2df13cdab2
Kombanoid <fedorlebed@hotmail.com>
2016-02-22 02:35:01 +0300

    Welcome

--- app/views/Welcome.jsx
+++ app/views/Welcome.jsx
@@ -4,12 +4,10 @@ export default function Welcome({ context }) {
   return (
     <div>
       <div className='jumbotron'>
-        <h1>Introduction</h1>
+        <h1>
+          <b>RISP</b> is a sport metronom<br/>
+        </h1>
         <p>
-          RISP helps you doing your exercises.<br />
-          It optimizes your time, forming your plan.<br />
-          You aren’t digressing with your exercises.<br />
-          You can absolutely immerse yourself in sport.
         </p>
         <button className="btn btn-primary btn-lg" onClick={() => { context.redirect('#/Editor'); }}>Start exercises now</button>
       </div>
