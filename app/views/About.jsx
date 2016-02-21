cd5d054a712322f57492bdd0029954eb18c27bdb
SergeyL <sergeytest2014@gmail.com>
2016-02-22 00:17:13 +0300

    clearing code

---
+++ app/views/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function About({ context }) {
+  return (
+    <div>
+      <div className='jumbotron'>
+        <h1>About</h1>
+        <p>
+          The RISP application was created during <a>Pluralsight Hackathon 2016 on 20 and 21 Febriary by the team of four.</a>.
+          Read the short <a href='#/Wellcom'>introduction</a> to the usage.
+        </p>
+      </div>
+    </div>
+  );
+}
